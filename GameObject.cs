229177a575986010f7ab624b05660d9f59d38bd9
nagymathev <nagymathev@gmail.com>
2023-03-22 20:43:30 +0100

    init and basic gameobject with properties

---
+++ GameObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace Asteroids;
+
+public class GameObject
+{
+    private Vector2 _position;
+    private Vector2 _direction;
+    private Vector2 _forward;
+    private float _speed;
+
+    public Vector2 Position
+    {
+        get => _position;
+        set => _position = value;
+    }
+
+    public Vector2 Direction
+    {
+        get => _direction;
+        set => _direction = value;
+    }
+
+    public Vector2 Forward => _forward;
+
+    public float Speed
+    {
+        get => _speed;
+        set => _speed = value;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using Raylib_cs;
+
+namespace Asteroids;
+
+static class Asteroids
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
