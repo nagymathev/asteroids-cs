9a1a48f5e1c9869b32eaf2702356d37420865ac3
nagymathev <nagymathev@gmail.com>
2023-03-22 21:51:34 +0100

    player testing

--- GameObject.cs
+++ GameObject.cs
@@ -1,13 +1,38 @@
 ﻿using System.Numerics;
+using static System.Single;
 
 namespace Asteroids;
 
 public class GameObject
 {
     private Vector2 _position;
-    private Vector2 _direction;
+    private Vector2 _velocity;
     private Vector2 _forward;
     private float _speed;
+
+    public GameObject(Vector2 pos, float speed)
+    {
+        _position = pos;
+        _speed = speed;
+        _velocity = new Vector2(0, 0);
+        _forward = new Vector2(0, 1);
+    }
+
+    // public Vector2 RotateByDegrees(float deg)
+    // {
+    //     float _rad = (deg / 180) * Pi;
+    //     float _x = _forward.X * Cos(_rad) - _forward.Y * Sin(_rad);
+    //     float _y = _forward.X * Sin(_rad) + _forward.Y * Cos(_rad);
+    //     return new Vector2(_x, _y);
+    // }
+    
+    public void RotateByDegrees(float deg)
+    {
+        float _rad = deg * Pi / 180;
+        _forward.X = _forward.X * Cos(_rad) - _forward.Y * Sin(_rad);
+        _forward.Y = _forward.X * Sin(_rad) + _forward.Y * Cos(_rad);
+        _forward = Vector2.Normalize(_forward);
+    }
 
     public Vector2 Position
     {
@@ -15,13 +40,17 @@ public class GameObject
         set => _position = value;
     }
 
-    public Vector2 Direction
+    public Vector2 Velocity
     {
-        get => _direction;
-        set => _direction = value;
+        get => _velocity;
+        set => _velocity = value;
     }
-
-    public Vector2 Forward => _forward;
+    
+    public Vector2 Forward
+    {
+        get => _forward;
+        set => _forward = value;
+    }
 
     public float Speed
     {
--- Main.cs
+++ Main.cs
@@ -1,4 +1,7 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.KeyboardKey;
 
 namespace Asteroids;
 
@@ -6,14 +9,33 @@ static class Asteroids
 {
     public static void Main()
     {
-        Raylib.InitWindow(800, 480, "Hello World");
+        InitWindow(800, 480, "Asteroids");
 
-        while (!Raylib.WindowShouldClose())
+        GameObject player = new GameObject(new Vector2(GetScreenWidth() / 2, GetScreenHeight() / 2), 10.0f);
+
+        while (!WindowShouldClose())
         {
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.BLACK);
-            Raylib.EndDrawing();
+            if (IsKeyDown(KEY_W)) player.Velocity -= player.Forward * player.Speed * GetFrameTime();
+            if (IsKeyDown(KEY_S)) player.Velocity += player.Forward * player.Speed * GetFrameTime();
+            if (IsKeyDown(KEY_A)) player.RotateByDegrees(-100 * GetFrameTime());
+            if (IsKeyDown(KEY_D)) player.RotateByDegrees(100 * GetFrameTime());
+            player.Velocity = Vector2.Clamp(player.Velocity, new Vector2(-10, -10), new Vector2(10, 10));
+            player.Position += player.Velocity * player.Speed * GetFrameTime();
+            
+            BeginDrawing();
+                ClearBackground(Color.BLACK);
+                DrawCircleV(player.Position, 2.0f, Color.BLUE);
+                DrawText(player.Forward.ToString(), 100, 100, 15, Color.GRAY);
+                DrawLineV(player.Position, player.Position - player.Forward * 25, Color.RED);
+                DrawTriangleLines(
+                    player.Position - new Vector2(0, 30),
+                    player.Position + new Vector2(-15, 25),
+                    player.Position + new Vector2(15, 25),
+                    Color.GOLD
+                    );
+            
+            EndDrawing();
         }
-        Raylib.CloseWindow();
+        CloseWindow();
     }
 }
